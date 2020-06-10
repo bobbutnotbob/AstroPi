7d00554a210d112a8ee92e57634f3b2b135e6b09
bobbutnotbob <burnin_rubber@live.com>
2020-06-10 14:13:59 +1200

    +Added dropdown with example text

--- app/src/main/java/com/example/astropi/MainActivity.java
+++ app/src/main/java/com/example/astropi/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.astropi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.planets_array, android.R.layout.simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+    }
+
+    public void sendMessage(View view) {
+        // Do someth
     }
 }
