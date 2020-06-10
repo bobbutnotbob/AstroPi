17c4af514c0cfd7811393ccc8ec0b46768bf89bb
bobbutnotbob <burnin_rubber@live.com>
2020-06-10 20:09:49 +1200

    +Added a few more example dropdown selectors

--- app/src/main/java/com/example/astropi/MainActivity.java
+++ app/src/main/java/com/example/astropi/MainActivity.java
@@ -13,10 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+
+        Spinner aperture_dropdown = (Spinner) findViewById(R.id.aperture_dropdown);
+        Spinner exposure_dropdown = (Spinner) findViewById(R.id.exposure_dropdown);
+        Spinner iso_dropdown = (Spinner) findViewById(R.id.iso_dropdown);
+
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.planets_array, android.R.layout.simple_spinner_dropdown_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
+        aperture_dropdown.setAdapter(adapter);
+        exposure_dropdown.setAdapter(adapter);
+        iso_dropdown.setAdapter(adapter);
     }
 
     public void sendMessage(View view) {
