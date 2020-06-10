ac5cfc9fc57c260a77b0ba20bc88e1170e275749
bobbutnotbob <burnin_rubber@live.com>
2020-06-10 21:17:17 +1200

    *Moved button to bottom +Added button functionality

--- app/src/main/java/com/example/astropi/MainActivity.java
+++ app/src/main/java/com/example/astropi/MainActivity.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,14 +20,23 @@ public class MainActivity extends AppCompatActivity {
         Spinner exposure_dropdown = (Spinner) findViewById(R.id.exposure_dropdown);
         Spinner iso_dropdown = (Spinner) findViewById(R.id.iso_dropdown);
 
+        final Button select_settings = findViewById(R.id.select_settings);
+        select_settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (select_settings.getText() == "Testing 321") {
+                    select_settings.setText("Testing 123");
+                }else {
+                    select_settings.setText("Testing 321");
+                }
+            }
+        });
+
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.planets_array, android.R.layout.simple_spinner_dropdown_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
         aperture_dropdown.setAdapter(adapter);
         exposure_dropdown.setAdapter(adapter);
         iso_dropdown.setAdapter(adapter);
-    }
-
-    public void sendMessage(View view) {
-        // Do someth
     }
 }
